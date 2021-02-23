b080029a652f0519d76168f118b3f7fa496baa15
zizzlePop <koolskool903@gmail.com>
2021-02-23 13:43:53 +0000

    I don't remember what I changed, it's been a while, but I think I added the code for the dice rolling portion of the bot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+import random
 import discord 
 from discord.ext import commands
 from keep_alive import keep_alive
@@ -24,6 +25,14 @@ async def reverse_string(ctx, mssg_rev):
     return
   
   await ctx.send(mssg_rev[::-1])
+
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
 
 keep_alive()
 TOKEN = os.environ.get('DISCORD_TOKEN')
