a765867b5c2b98e24b61db92aa08b6111791e3b6
Joe P <koolskool903@gmail.com>
2021-03-03 14:11:07 +0000

    Changed trivia to embed, commented out code section to be used as example

--- main.py
+++ main.py
@@ -33,23 +33,20 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
     ]
     await ctx.send(', '.join(dice))
 
-@bot.command(name='trivia', help='Simulates a trivia game.')
+"""
+
+@bot.command(name='trivia', help='Simulates a trivia game. (unfinished beta)')
 async def trivia_funct(ctx):
   questions = ["What is the capitol of New Jersey?", "Which Star Wars movie did Boba Fett first appear in?", "What was the Japanese name for the NES?"]
   answers = ["Trenton", "Episode V", "Super Famicom"]
   prompt = random.choice(questions)
   questionIndex = questions.index(prompt)
 
-#  await ctx.send(prompt)
-
-#  reply = 
-
-#  if answers.index(reply) == questionIndex:
-#    ctx.send("Correct answer!")   
-#  else:
-#    ctx.send("Incorrect answer, please try again.")
-
+  embedVar = discord.Embed(title="Trivia", desc="", color = 0x00ff00)
+  embedVar.add_field(name="Question: ", value=prompt, inline=False)
+  await ctx.send(embed=embedVar)
   
+"""
 
 keep_alive()
 TOKEN = os.environ.get('DISCORD_TOKEN')
