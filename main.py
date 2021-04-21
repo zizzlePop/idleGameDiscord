6995efaa0b037f533a7456ba10acaeb13736a6de
zizzlePop <>
2021-04-21 13:08:09 +0000

    Error trying to use 2 different methods to control collection

--- main.py
+++ main.py
@@ -2,10 +2,18 @@
 import os
 import random
 import discord 
+import time
 from discord.ext import commands
 from keep_alive import keep_alive
 
+# Initialize bot
 bot = commands.Bot(command_prefix='!')
+
+# Game variables
+limitstart = 0
+limitend = 0
+currencyScore = 0
+THREE_HOURS = 10800
 
 # Login event
 @bot.event
@@ -19,7 +27,7 @@ async def on_ready():
     f'{guild.name}(id: {guild.id})'
   )
 
-
+# Reverse a string command
 @bot.command(name='reverse', help='Reverses the message in the command')
 async def reverse_string(ctx, *, mssg_rev):
   if ctx.author == bot.user:
@@ -35,6 +43,30 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
         for _ in range(number_of_dice)
     ]
     await ctx.send(', '.join(dice))
+
+# Begin currency generation and initialize game
+@bot.command(name='startgame')
+async def start_game(ctx):
+  limitstart = time.perf_counter()
+  embedVar = discord.Embed(title="Welcome traveler!", description="Your new adventure begins today! The adventurer's guild needs your help doing jobs across town, so you can begin making a wage that will allow you to buy more gear and fight monsters. Your time starts now!", color=0xcccc00)
+  await ctx.send(embed=embedVar)
+
+@bot.command(name='claim')
+async def claim_coin(ctx):
+  limitend = time.perf_counter()
+
+  conditionTrue = discord.Embed(title="Claimed Successfully!", color=0x00ff00)
+  conditionTrue.add_field(value="Your new balance is %d." % (currencyScore), inline=False)
+  conditionTrue.add_field(value="Come back again in 3 hours!", inline=False)
+  conditionFalse = discord.Embed(title="Claim Unsuccessful!", color=0xff0000)
+  conditionFalse.add_field(value="Please try again in %d minutes" % (THREE_HOURS - (limitend - limitStart)), inline=False)
+
+  if limitend-limitstart == THREE_HOURS:
+    currencyScore += 180
+    await ctx.send(conditionTrue)
+  else:
+    await ctx.send(conditionFalse)
+
 
 keep_alive()
 TOKEN = os.environ.get('DISCORD_TOKEN')
